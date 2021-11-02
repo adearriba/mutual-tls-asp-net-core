0a2bfe0e4e46e64895de78ea7f51ebe46b787843
Alejandro de Arriba <adearriba@outlook.com>
2021-11-02 19:00:31 +0100

    Create IClient.cs

---
+++ MutualTLS.Client/Clients/IClient.cs
@@ -0,0 +1,14 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MutualTLS.Client.Clients
+{
+    public interface IClient
+    {
+        IRestResponse Execute(IRestRequest request);
+    }
+}
