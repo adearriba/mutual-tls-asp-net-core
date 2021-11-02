2a7234caac158f0fb181dc2f7fc564dfffc71749
Alejandro de Arriba <adearriba@outlook.com>
2021-11-02 19:00:36 +0100

    Create ClientWithoutCertificate.cs

---
+++ MutualTLS.Client/Clients/ClientWithoutCertificate.cs
@@ -0,0 +1,26 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MutualTLS.Client.Clients
+{
+    public class ClientWithoutCertificate : IClient
+    {
+        private IRestClient _client = null;
+
+        public ClientWithoutCertificate()
+        {
+            _client = new RestClient("https://localhost:5001");
+        }
+
+        public IRestResponse Execute(IRestRequest request)
+        {
+            return _client.Execute(request);
+        }
+    }
+}
